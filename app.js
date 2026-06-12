6c26114f3c0e9e51b475c974ad49bd94c4b3472b
aakankshakpoojari <aakankshakpoojari265@gmail.com>
2026-06-12 18:59:02 +0530

    so far what sir's done

---
+++ app.js
@@ -0,0 +1,119 @@
+const express = require('express')
+const path = require('path')
+const db = require('./db')
+const session = require('express-session');
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+app.use(
+    session({
+        secret: "librarysecret",
+        resave: false,
+        saveUninitialized: false
+    })
+)
+
+app.get("/", (req, res) => {
+    res.render("login", { error: null });
+});
+
+app.post("/login", (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    db.get(`SELECT * FROM LIBRARIANS WHERE EMAIL=? AND PASSWORD = ? `, [email, password], (err, row) => {
+        if (row) {
+            req.session.librarians = row
+            return res.redirect('/dashboard');
+        } else {
+            res.render("login", { error: "Invalid Credentials" })
+            console.log(err);
+        }
+    })
+})
+
+function isLoggedIn(req, res, next) {
+    if (req.session.librarians) {
+        next();
+    }
+    else {
+        res.redirect('/')
+    }
+}
+
+app.get('/dashboard', isLoggedIn, (req, res) => {
+    db.get(`SELECT COUNT(*) AS TOTAL_BOOKS FROM BOOKS`, (err, books) => {
+        db.get(`SELECT COUNT(*) AS TOTAL_BORROWED FROM BORROWEDBOOKS`, (err, borrowed) => {
+
+            db.get(`SELECT COUNT(*) AS TOTAL_VISITS FROM LIBRARYVISITS`, (err, visits) => {
+                res.render("dashboard", {
+                    librarian: req.session.librarians,
+                    totalBooks: books.TOTAL_BOOKS,
+                    totalBorrowed: borrowed.TOTAL_BORROWED,
+                    totalVisits: visits.TOTAL_VISITS
+                })
+            })
+        })
+    })
+})
+
+app.get('/books',(req,res) => {
+    db.all(`SELECT * FROM BOOKS`, (err,rows) => {
+        res.render("books",{books:rows})
+    });
+});
+
+app.get('/books/add',(req,res) => {
+    res.render("addBook");
+});
+
+app.post('/books/add',(req,res)=>{
+    const title=req.body.title;
+    const author=req.body.author;
+    const quantity=req.body.quantity;
+
+    db.run(`INSERT INTO BOOKS(title,author,quantity)values(?,?,?)`,[title,author,quantity],(err)=>{
+        if(!err) res.redirect('/books');
+        else console.log(err.message);
+    })
+})
+
+app.get("/books/delete/:id",(req,res)=>{
+    db.run(`DELETE FROM BOOKS WHERE ID=?`,[req.params.id],(err)=>{
+        if(!err){
+            res.redirect('/books');
+        }
+    })
+})
+
+app.get("/books/edit/:id",(req,res)=>{
+    db.get(`select * from books where id=?`,[req.params.id],(err,row)=>{
+        if(!err){
+            res.render("editBook",{book:row});
+        }
+    })
+})
+
+app.post("/books/edit/:id",(req,res)=>{
+    const title=req.body.title;
+    const author=req.body.author;
+    const quantity=req.body.quantity;
+
+    db.run(`update books set title=? , author=? , quantity=? where id=?`,[title,author,quantity,req.params.id],(err)=>{
+        if(!err) response.redirect('/books');
+    })
+})
+
+app.get("/logout", (req, res) => {
+    req.session.destroy(() => {
+        return res.redirect('/');
+    });
+});
+
+app.listen(5000, () => {
+    console.log("Running");
+});
